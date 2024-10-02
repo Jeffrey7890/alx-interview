c0206c9e7e531712db92b9359ed885fb7df2c5e8
Jeffrey7890 <jeffrey.ogechi@gmail.com>
2024-10-02 09:22:46 +0000

    pycodestyle error

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,19 +2,21 @@
 
 """ implement factorial of number """
 
+
 def factorial(n):
     """ Get factorial of number """
     if n == 0:
-        return (1);
+        return (1)
     return n * factorial(n-1)
+
 
 def combination(n, k):
     """ Get combination of number """
     return (factorial(n)//(factorial(k)*factorial(n-k)))
 
+
 def pascal_triangle(n):
     """ Pascals triangle """
     if n <= 0:
         return []
-    return [[combination(i,j) for j in range(i+1)] for i in range(n)]
-
+    return [[combination(i, j) for j in range(i+1)] for i in range(n)]
