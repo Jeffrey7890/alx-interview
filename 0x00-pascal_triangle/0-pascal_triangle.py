74ec0d8566c3392f307ec2412e992694f618aaaa
Jeffrey7890 <jeffrey.ogechi@gmail.com>
2024-10-02 09:48:39 +0100

    pascals triangle implementation

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" implement factorial of number """
+
+def factorial(n):
+    """ Get factorial of number """
+    if n == 0:
+        return (1);
+    return n * factorial(n-1)
+
+def combination(n, k):
+    """ Get combination of number """
+    return (factorial(n)//(factorial(k)*factorial(n-k)))
+
+def pascal_triangle(n):
+    """ Pascals triangle """
+    return [[combination(i,j) for j in range(i+1)] for i in range(n+1)]
+
