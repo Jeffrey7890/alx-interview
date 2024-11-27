8c457f67ad0525f26d030e4d14381ac6babc3711
Jeffrey7890 <d3v3loperpy@gmail.com>
2024-11-27 12:21:20 +0000

    making some changes

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" making change interview question """
+
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
+
+    # Initialize an array to store the minimum number of coins for each amount
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
+
